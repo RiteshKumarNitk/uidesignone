e1b6420b3782df18d4a7b14e1054c085509d779c
Ritesh Kumar <rav332211@gmail.com>
2025-06-16 07:40:06 +0530

    calulator working

--- lib/view/calculator.dart
+++ lib/view/calculator.dart
@@ -1,33 +1,132 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/cupertino.dart';
+  import 'package:flutter/material.dart';
 
-class CalculatorView extends StatelessWidget {
-  const CalculatorView({super.key});
+  class CalculatorView extends StatefulWidget {
+    const CalculatorView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding:  EdgeInsets.all(32.0),
-      child: Column(
-        children: [
-          TextField(
-            keyboardType: TextInputType.number,
-            autofocus: true,
-            decoration: InputDecoration(
-              focusedBorder: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black, width: 3.0),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(color: Colors.black, width: 3.0),
-                borderRadius: BorderRadius.circular(20),
-             
-              ),
-              hintText: "Enter Number",
-              hintStyle: TextStyle(color: Colors.black),
-            ),
-          ),
-        ],
-      ),
-    );
+    @override
+    State<CalculatorView> createState() => _CalculatorViewState();
   }
-}
+
+  class _CalculatorViewState extends State<CalculatorView> {
+    int x = 0;
+    int y = 0;
+    num z = 0;
+
+    final displayOneController = TextEditingController();
+    final displayTwoController = TextEditingController();
+
+    @override
+    void initState(){
+      super.initState();
+      displayOneController.text = x.toString();
+      displayTwoController.text = y.toString();
+    }
+
+    num _getNumber(TextEditingController controller){
+      return num.tryParse(controller.text) ??0;
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            CalculatorDisplay(hint: "Enter First Number", controller:displayOneController),
+            const SizedBox(height: 30),
+            CalculatorDisplay(hint: "Enter Last Number", controller:displayTwoController),
+            Text(
+              z.toString(),
+              style: const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            // const Spacer(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  onPressed: () { 
+                    setState(() {
+                    final a = _getNumber(displayOneController);
+                    final b = _getNumber(displayTwoController);
+                    z=a+b;
+                    });
+                  },
+                  child: Icon(CupertinoIcons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () { 
+                    setState(() {
+                    final a = _getNumber(displayOneController);
+                    final b = _getNumber(displayTwoController);
+                    z=a-b;
+                    });
+                  },
+                  child: Icon(CupertinoIcons.minus),
+                ),
+                FloatingActionButton(
+                  onPressed: () { 
+                    setState(() {
+                    final a = _getNumber(displayOneController);
+                    final b = _getNumber(displayTwoController);
+                    z=a*b;
+                    });
+                  },
+                  child: Icon(CupertinoIcons.multiply),
+                ),
+                FloatingActionButton(
+                  onPressed: () { 
+                    setState(() {
+                    final a = _getNumber(displayOneController);
+                    final b = _getNumber(displayTwoController);
+                    z = b != 0 ? a / b : 0; 
+                    });
+                  },
+                  child: Icon(CupertinoIcons.divide),
+                ),
+                FloatingActionButton.extended(
+                  onPressed: () { 
+                    setState(() {
+                      x =0;
+                      y =0;
+                      z =0;
+                      displayOneController.clear();
+                      displayTwoController.clear();
+                    });
+                    z= x/y;
+                  },
+                  label: const Text("Clear"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+  }
+
+  class CalculatorDisplay extends StatelessWidget {
+    const CalculatorDisplay({super.key, this.hint = "Enter Full Name", required this.controller});
+    final String? hint;
+    final TextEditingController controller;
+    @override
+    Widget build(BuildContext context) {
+      return TextField(
+        keyboardType: TextInputType.number,
+        controller:controller,
+        autofocus: true,
+        decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 3.0),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black, width: 3.0),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          hintText: hint,
+          hintStyle: TextStyle(color: Colors.black),
+        ),
+      );
+    }
+  }
